8dbd80b18a5e03e33e4d4d7edf6ea34c37aa9dcf
BoniFederico <67047565+BoniFederico@users.noreply.github.com>
2020-06-17 10:53:49 +0200

    Classe per rappresentare oggetti di tipo Lezione

---
+++ Lezione.java
@@ -0,0 +1,20 @@
+public class Lezione {
+    String data;
+    int ora;
+    int locazione;
+    String materia;
+    int insegnante;
+    String argomento;
+
+    public Lezione() {}
+
+    public Lezione(String data, int ora, int locazione, String materia, int insegnante, String argomento) {
+
+        this.data = data;
+        this.ora = ora;
+        this.locazione = locazione;
+        this.materia = materia;
+        this.insegnante = insegnante;
+        this.argomento = argomento;
+    }
+}
